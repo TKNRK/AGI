ff8d444b841c4b15ebd2d8a3cddcdf7f2f61029f
takano <jugri-7441@outlook.jp>
2016-09-26 12:31:21 +0900

    upload

--- src/agiTest.py
+++ src/agiTest.py
@@ -3,26 +3,21 @@
 import numpy as np
 import sympy as sp
 from scipy import optimize as opt
-from sympy.utilities.lambdify import lambdify, lambdastr
+from sympy.utilities.lambdify import lambdify
 from sympy import Matrix, MatrixSymbol, refine, Identity, Q
 from tkinter import *
 import time
 
 #  initialize（画像処理関係）
-WIDTH = HEIGHT = 700  # window's width and height
+_width = _height = 700  # window's width and height
 width = height = 500  # canvas's width and height
 
-# Calculation Space (C) : 射影更新時の計算における実際の座標系
-# Drawing Space (D) : tkinter の描画における画面の座標系
-
-# transfer 'Calculation Space' to 'Drawing Space'
-def c2d(pnt, bool):  # データの座標を射影する平面の画面サイズに合わせる
-	if(bool): return width * (pnt + boundingH / 2) / boundingH + (WIDTH - width) / 2
-	else: return (height - 100) * (boundingV / 2 - pnt) / boundingV + (HEIGHT - height) / 2
-# transfer 'Drawing Space' to 'Calculation Space'
-def d2c(pnt, bool):  # 射影された平面上の座標を元のスケールに戻す
-	if (bool): return boundingH * ((pnt - (WIDTH - width) / 2) - width / 2) / width
-	else: return boundingV * ((pnt - (HEIGHT - height) / 2) - (height - 100) / 2) / (100 - height)
+def scale(pnt,bool):  # データの座標を射影する平面の画面サイズに合わせる
+	if(bool): return width * (pnt + boundingH / 2) / boundingH + (_width - width) / 2
+	else: return (height - 100) * (boundingV / 2 - pnt) / boundingV + (_height - height) / 2
+def unscale(pnt,bool):  # 射影された平面上の座標を元のスケールに戻す
+	if (bool): return boundingH * ((pnt - (_width - width) / 2) - width / 2) / width
+	else: return boundingV * ((pnt - (_height - height) / 2) - (height - 100) / 2) / (100 - height)
 
 # initialize(データ処理関係)
 # load adjacency and multi-dimensional space
@@ -50,12 +45,12 @@ boundingV = 0  # Vertical boundary
 boundingH = 0  # Horizontal boundary
 
 def scale(pnt,bool):
-	if(bool): return width * (pnt + boundingH / 2) / boundingH + (WIDTH - width) / 2
-	else: return (height - 100) * (boundingV / 2 - pnt) / boundingV + (HEIGHT - height) / 2
+	if(bool): return width * (pnt + boundingH / 2) / boundingH + (_width - width) / 2
+	else: return (height - 100) * (boundingV / 2 - pnt) / boundingV + (_height - height) / 2
 
 def unscale(pnt,bool):
-	if (bool): return boundingH * ((pnt - (WIDTH - width) / 2) - width / 2) / width
-	else: return boundingV * ((pnt - (HEIGHT - height) / 2) - (height - 100) / 2) / (100 - height)
+	if (bool): return boundingH * ((pnt - (_width - width) / 2) - width / 2) / width
+	else: return boundingV * ((pnt - (_height - height) / 2) - (height - 100) / 2) / (100 - height)
 
 def update_points():
     global Pos_origin, boundingH, boundingV
@@ -63,48 +58,41 @@ def update_points():
     boundingH = max([np.amax(Pos_origin[:,0]), abs(np.amin(Pos_origin[:,0]))]) * 2
     boundingV = max([np.amax(Pos_origin[:,1]), abs(np.amin(Pos_origin[:,1]))]) * 2
     for i in range(node_num):
-        Pos_scaled[0,i] = c2d(Pos_origin[i, 0], True);Pos_scaled[1, i] = c2d(Pos_origin[i, 1], False)
+        Pos_scaled[0,i] = scale(Pos_origin[i,0], True);Pos_scaled[1,i] = scale(Pos_origin[i,1], False)
 
 update_points()
 
 print("init: ready")
 
 # sympy
-"""
 a1,b1,c1,a2,b2,c2,t,s = sp.symbols('a1 b1 c1 a2 b2 c2 t s')   # variables
-x_pre,y_pre,x_new,y_new,f0_norm = sp.symbols('x_pre y_pre x_new y_new f0_norm')  # values
-var = (x_pre,y_pre,x_new,y_new,f0_norm,a1,b1,c1,a2,b2,c2,s,t)
+x_pre,y_pre,x_new,y_new,p_norm = sp.symbols('x_pre y_pre x_new y_new p_norm')  # values
+var = (x_pre,y_pre,x_new,y_new,p_norm,a1,b1,c1,a2,b2,c2,t,s)
 
-f = np.array([
-	a1*a1 + b1*b1 + c1*c1 - 1,
-    a2*a2 + b2*b2 + c2*c2 - 1,
-    a1*a2 + b1*b2 + c1*c2,
+f = Matrix([
+	a1*a1 + b1*b1 + c1*c1*p_norm + 2*(a1*c1*x_pre + b1*c1*y_pre) - 1,
+    a2*a2 + b2*b2 + c2*c2*p_norm + 2*(a2*c2*x_pre + b2*c2*y_pre) - 1,
+    a1*a2 + b1*b2 + c1*c2*p_norm + x_pre*(a1*c2 + c1*a2) + y_pre*(b1*c2 + c1*b2),
 	s*s + t*t - 1,
-    a1*s + b1*t - s,
-    a2*s + b2*t - t,
-    f0_norm*c1 + x_pre*a1 + y_pre*b1 - x_new,
-    f0_norm*c2 + x_pre*a2 + y_pre*b2 - y_new
+    s*(a1 + c1*x_pre - 1) + t*(b1 + c1*y_pre),
+    s*(a2 + c2*x_pre) + t*(b2 + c2*y_pre - 1),
+    a1*x_pre + b1*y_pre + c1*p_norm - x_new,
+    a2*x_pre + b2*y_pre + c2*p_norm - y_new
 ])
 
-func = sum(f ** 2)
+func = sp.Matrix.norm(f)
 lam_f = lambdify(var, func, 'numpy')
-print(lambdastr(var,func))
-"""
-lam_f = lambda x_pre,y_pre,x_new,y_new,f0_norm,a1,b1,c1,a2,b2,c2,s,t: \
-    ((s**2 + t**2 - 1)**2 + (a1*a2 + b1*b2 + c1*c2)**2 + (a1*s + b1*t - s)**2 + (a2*s + b2*t - t)**2 +
-     (a1**2 + b1**2 + c1**2 - 1)**2 + (a2**2 + b2**2 + c2**2 - 1)**2 + (a1*x_pre + b1*y_pre + c1*f0_norm - x_new)**2 +
-     (a2*x_pre + b2*y_pre + c2*f0_norm - y_new)**2)
 
-def lam(x_pre,y_pre,x_new,y_new,f0_norm):
-    return lambda a1,b1,c1,a2,b2,c2,s,t: \
-        lam_f(x_pre,y_pre,x_new,y_new,f0_norm,a1,b1,c1,a2,b2,c2,s,t)
+def lam(x_pre,y_pre,x_new,y_new,p_norm):
+    return lambda a1,b1,c1,a2,b2,c2,t,s: \
+        lam_f(x_pre,y_pre,x_new,y_new,p_norm,a1,b1,c1,a2,b2,c2,t,s)
 
 arr_init = np.array([1, 0, 0, 0, 1, 0, 1, 1])
 print("lambda: ready")
 
 ######## Graph Drawing ########
 root = Tk()
-w = Canvas(root, width=WIDTH, height=HEIGHT, bg='White')
+w = Canvas(root, width=_width, height=_height, bg='White')
 w.pack()
 circles = []
 lines = []
@@ -119,17 +107,18 @@ for i in range(node_num):
 # 移動
 def move_node(event):
     global Es
-    x2 = d2c(event.x, True)
-    y2 = d2c(event.y, False)
+    x2 = unscale(event.x,True)
+    y2 = unscale(event.y,False)
     thisID = event.widget.find_withtag(CURRENT)[0] - (edge_num+1)
-    f0 = MDS[thisID] - (Pos_origin[thisID,0]*Es[:,0] + Pos_origin[thisID,1]*Es[:,1])
-    Es[:, 2] = f0 / np.linalg.norm(f0)
-    f2 = lam(Pos_origin[thisID,0], Pos_origin[thisID,1], x2, y2, np.linalg.norm(f0))
+    p_p = MDS[thisID].dot(MDS[thisID])
+    Es[:,2] = MDS[thisID]
+    f2 = lam(Pos_origin[thisID,0],Pos_origin[thisID,1],x2, y2, p_p)
     def g(args): return f2(*args)
     res = opt.minimize(g, arr_init, method='L-BFGS-B')
     print(res)
     if (res.success):
         Coefficient = res.x[0:6].reshape(2, 3)
+        print(Coefficient)
         Es[:, 0:2] = Es.dot(Coefficient.T)
         update_points()
         for i in range(node_num):
